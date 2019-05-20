fac90c06a98ab1b5b64ccdf6e4a59ff4c3f38450
ylisovskyi <Zezar54619520>
2019-05-20 17:46:47 +0300

    Add roles choice Add template table for orders Add client page Add current_user

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "computer_company.db"))
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config['user'] = None
 
 db = SQLAlchemy(app)
 
@@ -41,18 +42,49 @@ class User(UserMixin, db.Model):
     )
     username = db.Column(db.String(25), unique=True, nullable=False)
     password = db.Column(db.String(64), nullable=False)
+    role = db.Column(db.String(10), nullable=False)
+
+    def get_id(self):
+        return self.user_id
 
 
-# some protected url
+class Orders(db.Model):
+
+    __tablename__ = 'Orders'
+
+    order_id = db.Column(
+        db.Integer,
+        unique=True,
+        nullable=False,
+        primary_key=True,
+        autoincrement=True
+    )
+    client = db.Column(db.String(25), unique=True, nullable=False)
+    part = db.Column(db.String(64), nullable=False)
+    status = db.Column(db.String(10), nullable=False)
+
+
 @app.route('/')
 @login_required
 def home():
+    if not app.config['user']:
+        return redirect(url_for('login'))
+
     return Response("Hello World!")
 
 
 @app.route('/admin/')
+@login_required
 def admin_page():
-    return render_template('admin.html')
+    return render_template('admin_main.html')
+
+
+@app.route('/orders/')
+@login_required
+def orders_page():
+    orders = Orders.query
+    client = app.config['user'].username
+    return render_template('orders.html', orders=orders, current_user=client)
 
 
 @app.route("/register/", methods=["GET", "POST"])
@@ -60,12 +92,13 @@ def register_user():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['pass']
+        role = request.form['role']
         _user = User.query.filter_by(username=username).first()
         if _user:
             return 'Such user already exists'
 
         else:
-            _user = User(username=username, password=password)
+            _user = User(username=username, password=password, role=role)
             print _user.username
             db.session.add(_user)
             db.session.commit()
@@ -82,14 +115,19 @@ def login():
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
         if user is not None and user.password == password:
-            return render_template('admin.html')
+            login_user(user)
+            app.config['user'] = user
+            if user.role == 'admin':
+                return render_template('admin_main.html')
+
+            elif user.role == 'client':
+                return render_template('client_main.html')
 
         return 'No such user'
 
     return render_template('login.html')
 
 
-# somewhere to logout
 @app.route("/logout")
 @login_required
 def logout():
@@ -97,13 +135,11 @@ def logout():
     return Response('<p>Logged out</p>')
 
 
-# handle login failed
 @app.errorhandler(401)
 def page_not_found(e):
     return Response('<p>Login failed</p>')
 
 
-# callback to reload the user object
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
